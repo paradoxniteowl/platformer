62817cf68e62561593d0c241965bbbac092a7318
paradoxniteowl <148103497+paradoxniteowl@users.noreply.github.com>
2023-10-23 10:55:17 -0500

    End of video 4: Enemy Death
    Added Area2D around enemy and attached code that makes the enemy disappear if character steps on it.

--- player.gd
+++ player.gd
@@ -10,6 +10,9 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
+func _ready():
+	add_to_group("Player")
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
