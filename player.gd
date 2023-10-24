ffe26a34f67ee6280b9c909193fc22a5925ff653
paradoxniteowl <148103497+paradoxniteowl@users.noreply.github.com>
2023-10-24 10:54:55 -0500

    End of video 5: Player death
    Added player death animation and code.

--- player.gd
+++ player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+var is_dying = false
 var is_jumping = false
 
 const SPEED = 300.0
@@ -9,12 +10,16 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var death_timer = $death_timer
 
 func _ready():
 	add_to_group("Player")
+	death_timer.connect("timeout", Callable(self, "_on_DeathTimer_timeout"))
 
 func _physics_process(delta):
-	# Add the gravity.
+	if is_dying:
+		return
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
@@ -37,6 +42,9 @@ func _physics_process(delta):
 	move_and_slide()
 
 func update_animation(direction):
+	if is_dying:
+		return
+		
 	if is_jumping:
 		animated_sprite_2d.play("jump")
 	elif direction != 0:
@@ -47,5 +55,30 @@ func update_animation(direction):
 
 
 func _on_hitbox_body_entered(body):
-	if body.is_in_group("Enemy"):
-		get_tree().reload_current_scene()
+	if body.is_in_group("Enemy") and body.is_alive:
+		die()
+		
+func die():
+	if is_dying:
+		return
+		
+	is_dying = true
+	animated_sprite_2d.play("die")
+	await move_player_up_and_down()
+	get_tree().reload_current_scene()
+
+func move_player_up_and_down():
+	var start_position = position
+	var up_position = start_position + Vector2(0, -100)
+	var down_position = start_position + Vector2(0, 600)
+	
+	while position.y > up_position.y:
+		position.y -= 4
+		await get_tree().create_timer(0.01).timeout
+	
+	while position.y < down_position.y:
+		position.y += 4
+		await get_tree().create_timer(0.01).timeout
+		
+func on_DeathTimer_timeout():
+	get_tree().reload_current_scene()
