3723c0b768950411c220801c44ffc7ad0c2c26f7
paradoxniteowl <148103497+paradoxniteowl@users.noreply.github.com>
2023-10-27 10:26:53 -0500

    End of class 10/26: Creating 2nd level

--- world.gd
+++ world.gd
@@ -14,3 +14,8 @@ func _process(delta):
 func _on_interact_body_entered(body):
 	if body.is_in_group("Player"):
 		body.die()
+
+
+func _on_transition_body_entered(body):
+	if body.is_in_group("Player"):
+		get_tree().change_scene_to_file("res://underworld.tscn")
